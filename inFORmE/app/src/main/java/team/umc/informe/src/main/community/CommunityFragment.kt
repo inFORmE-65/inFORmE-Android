0d421803b8753fd284493331b98f74ea29bac4df
YuJungHyun <0703olivia@naver.com>
2022-08-16 22:53:35 +0900

    [FEAT] #4 커뮤니티 뷰

---
+++ inFORmE/app/src/main/java/team/umc/informe/src/main/community/CommunityFragment.kt
@@ -0,0 +1,18 @@
+package team.umc.informe.src.main.community
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import team.umc.informe.R
+import team.umc.informe.config.BaseFragment
+import team.umc.informe.databinding.FragmentCommunityBinding
+
+class CommunityFragment : BaseFragment<FragmentCommunityBinding>(FragmentCommunityBinding::bind, R.layout.fragment_community) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
